240ec19c2ef3672ae850b748e885a811bbfe6a72
danielrpg <daniel.fernandezgp@gmail.com>
2019-11-17 18:26:00 -0400

    Second lecture

---
+++ ReadKeyboard/src/ReadKeyboard.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Enter your name:")
+    var name = readLine()
+    println("Welcome mr. ${name}")
+
+    println("Enter your age:")
+    var age = readLine()
+
+    println("mr. ${name} your Age is ${age}" )
+}
