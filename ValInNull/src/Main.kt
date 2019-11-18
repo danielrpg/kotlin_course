b34f07aadce90e17f4f2c90242dca5ee1f1e9065
danielrpg <daniel.fernandezgp@gmail.com>
2019-11-17 23:02:35 -0400

    Changes with var and null values

---
+++ ValInNull/src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name:String = "Daniel"
+    var age:Int = 23
+
+    var fatherName:String?
+    fatherName = null
+
+    fatherName = "Dad"
+
+    var fatherAge:Int?
+    fatherAge = null
+
+    fatherAge = 33
+}
